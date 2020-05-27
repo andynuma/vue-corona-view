10bd9d574e328a96748162765c0ee703fb6c13f6
andynuma <igreen.re317v10@gmail.com>
2020-05-28 00:40:33 +0900

    fix

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
+  <div class="wrapper">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+
+<style scoped>
+.wrapper {
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 4%;
+}
+</style>
